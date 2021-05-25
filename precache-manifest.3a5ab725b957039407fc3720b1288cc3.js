b1938662e6979ff711717c54902bbd14df608594
Pascal <pascal.pothmann@gmail.com>
2021-05-25 15:59:17 +0200

    pwa added

---
+++ precache-manifest.3a5ab725b957039407fc3720b1288cc3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f060489c037096b4e7",
+    "url": "/open-balena-dashboard/css/about.91b2a54f.css"
+  },
+  {
+    "revision": "68ad887028f0da7a798f",
+    "url": "/open-balena-dashboard/css/app.22670584.css"
+  },
+  {
+    "revision": "507a3aa8457c66b5bba8",
+    "url": "/open-balena-dashboard/css/chunk-vendors.0f3d1e4e.css"
+  },
+  {
+    "revision": "342c527555cfdb9d35b5dd5629b3fa37",
+    "url": "/open-balena-dashboard/fonts/boxicons.342c5275.eot"
+  },
+  {
+    "revision": "86708f260a765d37cce51959dc4c097d",
+    "url": "/open-balena-dashboard/fonts/boxicons.86708f26.woff"
+  },
+  {
+    "revision": "8829e87f7fef25fef075707895401297",
+    "url": "/open-balena-dashboard/fonts/boxicons.8829e87f.ttf"
+  },
+  {
+    "revision": "b5e3cc0c6aa650c9e3ba523df059dc17",
+    "url": "/open-balena-dashboard/fonts/boxicons.b5e3cc0c.woff2"
+  },
+  {
+    "revision": "dad92ba1a4eed23afdbd3df53657fe9a",
+    "url": "/open-balena-dashboard/img/boxicons.dad92ba1.svg"
+  },
+  {
+    "revision": "ddf9752572f3cec6a94d040749bc4dd2",
+    "url": "/open-balena-dashboard/img/full_logo.ddf97525.svg"
+  },
+  {
+    "revision": "4f6cec61be8e89da9b35a06b64360e78",
+    "url": "/open-balena-dashboard/index.html"
+  },
+  {
+    "revision": "62f060489c037096b4e7",
+    "url": "/open-balena-dashboard/js/about.172520d1.js"
+  },
+  {
+    "revision": "68ad887028f0da7a798f",
+    "url": "/open-balena-dashboard/js/app.2fb5606f.js"
+  },
+  {
+    "revision": "507a3aa8457c66b5bba8",
+    "url": "/open-balena-dashboard/js/chunk-vendors.305b02b3.js"
+  },
+  {
+    "revision": "814d448ac5fee0ee534a28273cd31198",
+    "url": "/open-balena-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/open-balena-dashboard/robots.txt"
+  }
+]);
